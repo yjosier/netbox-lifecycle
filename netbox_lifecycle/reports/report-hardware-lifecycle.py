e4bbfbfce744724eeaf36d7a1f724db13b5e4048
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-05 14:27:29 +0200

    add field and filters

--- netbox_lifecycle/reports/report-hardware-lifecycle.py
+++ netbox_lifecycle/reports/report-hardware-lifecycle.py
@@ -1,40 +1,25 @@
 from dcim.choices import DeviceStatusChoices
-from dcim.models import Device
-#from extras.reports import Report
+from dcim.models import Device, Site, Location
 from extras.scripts import Script
 
-class HWEOLScript(Script):
-    description = "Check that every device has either an IPv4 or IPv6 primary address assigned"
+class HwEolScript(Script):
+    class Meta:
+        name = "Hardware End-Of-Life Script"
+        description = "Report End-Of-Life date of specified devices"
+
+    site_name = ObjectVar(
+        description="Site to pull devices from",
+        model=Site,
+        required=True
+    )
+    device_status = ChoiceVar(
+        DeviceStatusChoices, 
+        default=DeviceStatusChoices.STATUS_ACTIVE,
+        description="Device Status",
+        required=False
+    )
 
     def run(self, data, commit):
-        for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
-            self.log_info(f"The deviceType of this device is {device.device_type}", obj=device)
-            # intcount = 0
-            # for interface in device.interfaces.all():
-            #     if not interface.mgmt_only:
-            #         intcount += 1
-            # # There may be dumb devices with no interfaces so no IP addresses, that's OK
-            # if intcount == 0:
-            #     if device.primary_ip4_id is not None:
-            #         if device.primary_ip6_id is not None:
-            #             self.log_failure(device, "Device has primary IPv4 and IPv6 address but no interfaces")
-            #         else:
-            #             self.log_warning(device, "Device has missing primary IPv4 addresses but no interfaces")
-            #     else:
-            #         self.log_success(device)
-            # elif device.primary_ip4_id is None:
-            #     if device.device_type.is_child_device is True:
-            #         self.log_success(device)
-            #     else:
-            #         if device.primary_ip6_id is None:
-            #             self.log_failure(device, "Device is missing primary IPv4 and IPv6 address")
-            #         else:
-            #             self.log_warning(device, "Device is missing primary IPv4 addresses")
-            # else:
-            #     if device.device_type.is_child_device is True:
-            #         self.log_success(device)
-            #     else:
-            #         if device.primary_ip6_id is None:
-            #             self.log_info(device, "Device is missing primary IPv6 address")
-            #         else:
-            #             self.log_success(device)
+        for device in Device.objects.filter(status=data['device_status']):
+            for device in Device.objects.filter(site=data['site_name']):
+                self.log_info(f"The device type of this device is {device.device_type}", obj=device)
