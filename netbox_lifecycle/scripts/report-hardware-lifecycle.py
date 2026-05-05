bec747e64d4be0db616d9f54be4540b8cfb1f3ff
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-05 15:00:38 +0200

    remove meta definition

--- netbox_lifecycle/scripts/report-hardware-lifecycle.py
+++ netbox_lifecycle/scripts/report-hardware-lifecycle.py
@@ -5,17 +5,17 @@ from extras.scripts import Script
 class HwEolScript(Script):
     description = "Report End-Of-Life date of specified devices"
 
-    site_name = ObjectVar(
-        description="Site to pull devices from",
-        model=Site,
-        required=True
-    )
-    device_status = ChoiceVar(
-        DeviceStatusChoices, 
-        default=DeviceStatusChoices.STATUS_ACTIVE,
-        description="Device Status",
-        required=True
-    )
+    # site_name = ObjectVar(
+    #     description="Site to pull devices from",
+    #     model=Site,
+    #     required=True
+    # )
+    # device_status = ChoiceVar(
+    #     DeviceStatusChoices, 
+    #     default=DeviceStatusChoices.STATUS_ACTIVE,
+    #     description="Device Status",
+    #     required=True
+    # )
 
     def run(self, data, commit):
         for device in Device.objects.filter(status=data['device_status']):
