a7674fa49ad6f8344bb1946e9806e4ab2b751ba8
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-05 15:32:10 +0200

    change variables

--- netbox_lifecycle/scripts/report-hardware-lifecycle3.py
+++ netbox_lifecycle/scripts/report-hardware-lifecycle3.py
@@ -8,7 +8,7 @@ class HwEolScript(Script):
         description = "Report End-Of-Life date of specified devices"
         field_order = ['site_name', 'device_status']
 
-    site_name = ObjectVar(
+    site = ObjectVar(
         description="Site to pull devices from",
         model=Site,
         required=True
@@ -22,10 +22,10 @@ class HwEolScript(Script):
 
     def run(self, data, commit):
         self.log_info(f"In run function")
-        self.log_info(f"Device status from for {Device.objects.filter(status=data['device_status'])}")
+        self.log_info(f"Device status from form = {data['device_status']}")
         for device in Device.objects.filter(status=data['device_status']):
             self.log_info(f"In for loop")
             self.log_info(f"Device site name = {device.site.name}")
-            self.log_info(f"Device site name from form = {Device.objects.filter(site=data['site_name'])}")
-            if device.site.name == Device.objects.filter(site=data['site_name']):
+            self.log_info(f"Device site name from form = {data['site']}")
+            if device.site.name == Device.objects.filter(site=data['site']):
                 self.log_info(f"The device type of this device is {device.device_type}", obj=device)
