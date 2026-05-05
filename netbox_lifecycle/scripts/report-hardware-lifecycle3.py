978a364ce809a080a4bf121f17f8c1029afa7971
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-05 19:27:30 +0200

    add site var

--- netbox_lifecycle/scripts/report-hardware-lifecycle3.py
+++ netbox_lifecycle/scripts/report-hardware-lifecycle3.py
@@ -3,6 +3,12 @@ from dcim.models import Device, DeviceType, Site
 from extras.scripts import *
 from datetime import datetime
 
+# self.log_info(f"Device site name = {device.site.name}")
+# self.log_info(f"Device site name from form = {data['site']}")
+# self.log_info(f"Device status from form = {data['device_status']}")
+# self.log_info(f"The device type of this device is {device.device_type}", obj=device)
+
+
 class HwEolScript(Script):
     class Meta(Script.Meta):
         name = "Hardware End-Of-Life Script"
@@ -22,21 +28,16 @@ class HwEolScript(Script):
     )
 
     def run(self, data, commit):
-        output = ['name,model,eol']
-        # self.log_info(f"In run function")
-        # self.log_info(f"Device status from form = {data['device_status']}")
+        output = ['site,name,model,eol']
         for device in Device.objects.filter(status=data['device_status']):
-            # self.log_info(f"In for loop")
-            # self.log_info(f"Device site name = {device.site.name}")
-            # self.log_info(f"Device site name from form = {data['site']}")
             if device.site == data['site']:
-                #self.log_info(f"The device type of this device is {device.device_type}", obj=device)
                 eol = getattr(device.device_type.hardware_lifecycle.first(), 'end_of_support', None)
                 if eol:
                     self.log_info(f"The EoL for {device} is : {eol}", obj=device)
                 else:
                     self.log_info(f"The EoL for {device} is undefined.", obj=device)
                 attrs = [
+                    device.site,
                     device.name,
                     device.device_type.model,
                     eol.strftime("%d/%m/%Y")
