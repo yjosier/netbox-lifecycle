5ebfc49178a58ed744b634b96c4bb5fb2cff74fd
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-05 14:06:17 +0200

    test 1

--- netbox_lifecycle/reports/report-hardware-lifecycle.py
+++ netbox_lifecycle/reports/report-hardware-lifecycle.py
@@ -7,32 +7,33 @@ class DeviceIPReport(Report):
 
     def test_primary_ip4(self):
         for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
-            intcount = 0
-            for interface in device.interfaces.all():
-                if not interface.mgmt_only:
-                    intcount += 1
-            # There may be dumb devices with no interfaces so no IP addresses, that's OK
-            if intcount == 0:
-                if device.primary_ip4_id is not None:
-                    if device.primary_ip6_id is not None:
-                        self.log_failure(device, "Device has primary IPv4 and IPv6 address but no interfaces")
-                    else:
-                        self.log_warning(device, "Device has missing primary IPv4 addresses but no interfaces")
-                else:
-                    self.log_success(device)
-            elif device.primary_ip4_id is None:
-                if device.device_type.is_child_device is True:
-                    self.log_success(device)
-                else:
-                    if device.primary_ip6_id is None:
-                        self.log_failure(device, "Device is missing primary IPv4 and IPv6 address")
-                    else:
-                        self.log_warning(device, "Device is missing primary IPv4 addresses")
-            else:
-                if device.device_type.is_child_device is True:
-                    self.log_success(device)
-                else:
-                    if device.primary_ip6_id is None:
-                        self.log_info(device, "Device is missing primary IPv6 address")
-                    else:
-                        self.log_success(device)
+            self.log_info(device, "This is a test")
+            # intcount = 0
+            # for interface in device.interfaces.all():
+            #     if not interface.mgmt_only:
+            #         intcount += 1
+            # # There may be dumb devices with no interfaces so no IP addresses, that's OK
+            # if intcount == 0:
+            #     if device.primary_ip4_id is not None:
+            #         if device.primary_ip6_id is not None:
+            #             self.log_failure(device, "Device has primary IPv4 and IPv6 address but no interfaces")
+            #         else:
+            #             self.log_warning(device, "Device has missing primary IPv4 addresses but no interfaces")
+            #     else:
+            #         self.log_success(device)
+            # elif device.primary_ip4_id is None:
+            #     if device.device_type.is_child_device is True:
+            #         self.log_success(device)
+            #     else:
+            #         if device.primary_ip6_id is None:
+            #             self.log_failure(device, "Device is missing primary IPv4 and IPv6 address")
+            #         else:
+            #             self.log_warning(device, "Device is missing primary IPv4 addresses")
+            # else:
+            #     if device.device_type.is_child_device is True:
+            #         self.log_success(device)
+            #     else:
+            #         if device.primary_ip6_id is None:
+            #             self.log_info(device, "Device is missing primary IPv6 address")
+            #         else:
+            #             self.log_success(device)
