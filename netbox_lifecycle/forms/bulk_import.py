b49a18ad70ff94596df7c5042f1759fbb9efbfeb
Yoann Jonard <206922069+yjosier@users.noreply.github.com>
2026-05-19 15:34:22 +0200

    correct manufacturer from import

--- netbox_lifecycle/forms/bulk_import.py
+++ netbox_lifecycle/forms/bulk_import.py
@@ -226,7 +226,7 @@ class SoftwareAssignmentImportForm(NetBoxModelImportForm):
         model = SoftwareAssignment
         fields = (
             'software',
-            'vendor',
+            'manufacturer',
             'device',
             'virtual_machine',
             'description',
